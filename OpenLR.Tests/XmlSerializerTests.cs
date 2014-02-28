857d8b8bf3cd7f64d1e3a71ca9fba99581bb67cd
xivk <ben@osmsharp.com>
2014-02-28 20:13:01 +0100

    Updated output folder. Added serialization test.

---
+++ OpenLR.Tests/XmlSerializerTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace OpenLR.Tests
+{
+    /// <summary>
+    /// Holds some simple xml serialization/deserialization tests.
+    /// </summary>
+    [TestFixture]
+    public class XmlSerializerTests
+    {
+        /// <summary>
+        /// A simple XML deserialization test.
+        /// </summary>
+        [Test]
+        public void DeSerialize()
+        {
+            var xmlSerializer = new XmlSerializer(typeof(D2LogicalModel));
+            var deserialized = xmlSerializer.Deserialize(
+                Assembly.GetExecutingAssembly().GetManifestResourceStream("OpenLR.Tests.Data.HDF-BEL.XML"));
+
+            Assert.IsNotNull(deserialized);
+            Assert.IsInstanceOf<D2LogicalModel>(deserialized);
+        }
+    }
+}
