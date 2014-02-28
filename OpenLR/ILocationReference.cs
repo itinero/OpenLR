9aa209f67716d5902f5e6bc1f5daa1980995e059
xivk <ben@osmsharp.be>
2014-02-28 14:42:47 +0100

    Added location reference and location type.

---
+++ OpenLR/ILocationReference.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenLR
+{
+    /// <summary>
+    /// Abstract definition of a location reference in the OpenLR domain.
+    /// </summary>
+    public interface ILocationReference
+    {
+        /// <summary>
+        /// Gets the version.
+        /// </summary>
+        int Version { get; }
+
+        /// <summary>
+        /// Gets the id.
+        /// </summary>
+        string Id { get; }
+
+        /// <summary>
+        /// Checks if this location reference is valid.
+        /// </summary>
+        /// <returns></returns>
+        bool IsValid();
+    }
+}
---
+++ OpenLR/LocationType.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenLR
+{
+    /// <summary>
+    /// Enumerates the different possible location types in the OpenLR domain.
+    /// </summary>
+    public enum LocationType
+    {
+        /// <summary>
+        /// A circle location.
+        /// </summary>
+        Circle,
+        /// <summary>
+        /// A grid location.
+        /// </summary>
+        Grid,
+        /// <summary>
+        /// A line location.
+        /// </summary>
+        LineLocation,
+        /// <summary>
+        /// A POI with access point location.
+        /// </summary>
+        PoiWithAccessPoint,
+        /// <summary>
+        /// A point along line location.
+        /// </summary>
+        PointAlongLine,
+        /// <summary>
+        /// A rectangle location.
+        /// </summary>
+        Rectangle,
+        /// <summary>
+        /// A polygon location.
+        /// </summary>
+        Polygon
+    }
+}
