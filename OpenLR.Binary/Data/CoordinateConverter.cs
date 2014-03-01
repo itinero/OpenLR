d21e862f79401efd7ba95e3a43103c9fa0bc5af9
xivk <ben@osmsharp.com>
2014-03-01 16:56:13 +0100

    Updated PCL profiles. Finished binary coordinate decoding.

---
+++ OpenLR.Binary/Data/CoordinateConverter.cs
@@ -0,0 +1,51 @@
+﻿using OsmSharp.Math.Geo;
+
+namespace OpenLR.Binary.Data
+{
+    /// <summary>
+    /// Represents a coordinate endoder that encoded coordinates into the binary OpenLR format.
+    /// </summary>
+    public static class CoordinateConverter
+    {
+        /// <summary>
+        /// Decodes binary OpenLR coordinate data into a coordinate.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static GeoCoordinate Decode(byte[] data)
+        {
+            return new GeoCoordinate(
+                CoordinateConverter.DecodeDegrees(CoordinateConverter.DecodeInt24(data, 3)),
+                CoordinateConverter.DecodeDegrees(CoordinateConverter.DecodeInt24(data, 0)));
+        }
+
+        /// <summary>
+        /// Decodes a little-endian 24-bit signed integer from the given byte array into a 32-bit signed integer.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="startIndex"></param>
+        /// <returns></returns>
+        private static int DecodeInt24(byte[] data, int startIndex)
+        {
+            int result = ((byte)(data[startIndex + 0] & 127) * (1 << 16)) |    // Bottom 8 bits
+                (data[startIndex + 1] * (1 << 8)) |    // Next 8 bits, i.e. multiply by 256
+                (data[startIndex + 2] * (1 << 0));   // Next 8 bits, i.e. multiply by 65,536
+            // take into account the sign-bit.
+            if ((data[startIndex + 0] & (1 << 8 - 1)) != 0)
+            { // negative!
+                return -result;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Decodes a 24-bit integer-encoded coordinate.
+        /// </summary>
+        /// <param name="valueInt"></param>
+        /// <returns></returns>
+        private static double DecodeDegrees(int valueInt)
+        {
+            return (((valueInt - System.Math.Sign(valueInt) * 0.5) * 360) / 16777216);
+        }
+    }
+}
---
+++ OpenLR.Tests/Binary/Data/CoordinateEncodingTests.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using OpenLR.Binary.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenLR.Tests.Binary.Data
+{
+    /// <summary>
+    /// Holds some coordinate encoding/decoding tests.
+    /// </summary>
+    [TestFixture]
+    public class CoordinateEncodingTests
+    {
+        /// <summary>
+        /// Tests the simple decoding case.
+        /// </summary>
+        public void TestDecoding1()
+        {
+            double delta = 0.0001;
+
+            // manually specify a binary coordinate.
+            byte[] data = new byte[6];
+            data[0] = 0;
+            data[1] = 0;
+            data[2] = 0;
+            data[3] = 0;
+            data[4] = 0;
+            data[5] = 0;
+
+            // decode the coordinate.
+            var coordinate = CoordinateConverter.Decode(data);
+
+            Assert.IsNotNull(coordinate);
+            Assert.AreEqual(0, coordinate.Latitude);
+            Assert.AreEqual(0, coordinate.Longitude);
+
+            // manually specify a binary coordinate.
+            data[0] = 0;
+            data[1] = 0;
+            data[2] = 255;
+            data[3] = 0;
+            data[4] = 0;
+            data[5] = 255;
+
+            // decode the coordinate.
+            coordinate = CoordinateConverter.Decode(data);
+
+            Assert.IsNotNull(coordinate);
+            Assert.AreEqual(0.005460978, coordinate.Latitude, delta);
+            Assert.AreEqual(0.005460978, coordinate.Longitude, delta);
+
+            // manually specify a binary coordinate (see example in OpenLR whitepaper).
+            data[0] = 4;
+            data[1] = 91;
+            data[2] = 91;
+            data[3] = 35;
+            data[4] = 70;
+            data[5] = 245;
+
+            // decode the coordinate.
+            coordinate = CoordinateConverter.Decode(data);
+
+            Assert.IsNotNull(coordinate);
+            Assert.AreEqual(49.60851, coordinate.Latitude, delta);
+            Assert.AreEqual(6.12683, coordinate.Longitude, delta);
+        }
+    }
+}
