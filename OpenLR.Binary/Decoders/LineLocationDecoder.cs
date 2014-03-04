3fe49fefcae4c8097012f80c99a59d796a0cd11b
xivk <ben@osmsharp.com>
2014-03-04 22:20:59 +0100

    Finished decoding LineLocation. Closes #1.

--- OpenLR.Binary/Decoders/LineLocationDecoder.cs
+++ OpenLR.Binary/Decoders/LineLocationDecoder.cs
@@ -27,10 +27,57 @@ namespace OpenLR.Binary.Decoders
             first.FormOfWay = FormOfWayConvertor.Decode(data, 7, 5);
             first.LowestFunctionalRoadClassToNext = FunctionalRoadClassConvertor.Decode(data, 8, 0);
             first.BearingDistance = BearingConvertor.Decode(data, 8, 3);
+            first.DistanceToNext = data[9];
+
+            // calculate the intermediate points count.
+            var intermediateList = new List<LocationReferencePoint>();
+            int intermediates = (data.Length - 16) % 7;
+            int location = 10;
+            var reference = first.Coordinate; // the reference for the relative coordinates.
+            for(int idx = 0; idx < intermediates; idx++)
+            {
+                // create an intermediate point.
+                var intermediate = new LocationReferencePoint();
+                intermediate.Coordinate = CoordinateConverter.DecodeRelative(reference, data, location);
+                reference = intermediate.Coordinate;
+                location = location + 4;
+                intermediate.FuntionalRoadClass = FunctionalRoadClassConvertor.Decode(data, location, 2);
+                intermediate.FormOfWay = FormOfWayConvertor.Decode(data, location, 5);
+                location = location + 1;
+                intermediate.BearingDistance = BearingConvertor.Decode(data, location, 3);
+                intermediate.LowestFunctionalRoadClassToNext = FunctionalRoadClassConvertor.Decode(data, location, 0);
+                location = location + 1;
+                intermediate.DistanceToNext = data[location];
+                location = location + 1;
+
+                intermediateList.Add(intermediate);
+            }
+
+            // decode last location reference point.
+            var last = new LocationReferencePoint();
+            last.Coordinate = CoordinateConverter.DecodeRelative(reference, data, location);
+            location = location + 4;
+            last.FuntionalRoadClass = FunctionalRoadClassConvertor.Decode(data, location, 2);
+            last.FormOfWay = FormOfWayConvertor.Decode(data, location, 5);
+            location = location + 1;
+            last.BearingDistance = BearingConvertor.Decode(data, location, 3);
+            location = location + 1;                
 
             // create line location.
             var lineLocation = new LineLocation();
+            if (location < data.Length)
+            { // if present.
+                lineLocation.PositiveOffset = data[location];
+                location = location + 1;
+            }
+            if(location < data.Length)
+            { // if present.
+                lineLocation.NegativeOffset = data[location];
+                location = location + 1;
+            }
             lineLocation.First = first;
+            lineLocation.Intermediate = intermediateList.ToArray();
+            lineLocation.Last = last;
             return lineLocation;
         }
     }
--- OpenLR.Tests/Binary/LineLocationTests.cs
+++ OpenLR.Tests/Binary/LineLocationTests.cs
@@ -1,5 +1,7 @@
 ﻿using NUnit.Framework;
 using OpenLR.Binary.Decoders;
+using OpenLR.Locations;
+using OpenLR.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +21,8 @@ namespace OpenLR.Tests.Binary
         [Test]
         public void DecodeBase64Test()
         {
+            double delta = 0.0001;
+
             // define a base64 string we are sure is a line location.
             string stringData = "CwRbWyNG9RpsCQCb/jsbtAT/6/+jK1lE";
 
@@ -27,6 +31,32 @@ namespace OpenLR.Tests.Binary
             var location = decoder.Decode(stringData);
 
             Assert.IsNotNull(location);
+            Assert.IsInstanceOf<LineLocation>(location);
+            var lineLocation = (location as LineLocation);
+
+            // check first reference.
+            Assert.IsNotNull(lineLocation.First);
+            Assert.AreEqual(6.12683, lineLocation.First.Coordinate.Longitude, delta); // 6.12683°
+            Assert.AreEqual(49.60851, lineLocation.First.Coordinate.Latitude, delta); // 49.60851°
+            Assert.AreEqual(FunctionalRoadClass.Frc3, lineLocation.First.FuntionalRoadClass);
+            Assert.AreEqual(FormOfWay.MultipleCarriageWay, lineLocation.First.FormOfWay);
+            Assert.AreEqual(FunctionalRoadClass.Frc3, lineLocation.First.LowestFunctionalRoadClassToNext);
+
+            // check intermediates.
+            Assert.IsNotNull(lineLocation.Intermediate);
+            Assert.AreEqual(1, lineLocation.Intermediate.Length);
+            Assert.AreEqual(6.12838, lineLocation.Intermediate[0].Coordinate.Longitude, delta); // 6.12838°
+            Assert.AreEqual(49.60398, lineLocation.Intermediate[0].Coordinate.Latitude, delta); // 49.60398°
+            Assert.AreEqual(FunctionalRoadClass.Frc3, lineLocation.Intermediate[0].FuntionalRoadClass);
+            Assert.AreEqual(FormOfWay.SingleCarriageWay, lineLocation.Intermediate[0].FormOfWay);
+            Assert.AreEqual(FunctionalRoadClass.Frc5, lineLocation.Intermediate[0].LowestFunctionalRoadClassToNext);
+
+            // check second reference.
+            Assert.IsNotNull(lineLocation.Last);
+            Assert.AreEqual(6.12817, lineLocation.Last.Coordinate.Longitude, delta); // 6.12817°
+            Assert.AreEqual(49.60305, lineLocation.Last.Coordinate.Latitude, delta); // 49.60305°
+            Assert.AreEqual(FunctionalRoadClass.Frc5, lineLocation.Last.FuntionalRoadClass);
+            Assert.AreEqual(FormOfWay.SingleCarriageWay, lineLocation.Last.FormOfWay);
         }
     }
 }
