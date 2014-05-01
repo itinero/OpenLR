cac9d036479a0a0e4218b8824c744c0bc535b08b
xivk <ben@osmsharp.com>
2014-05-01 14:09:43 +0200

    Part one of the LineLocation referenced decoding.

--- OpenLR.OsmSharp/Decoding/GraphDecoder.cs
+++ OpenLR.OsmSharp/Decoding/GraphDecoder.cs
@@ -1,7 +1,13 @@
 ﻿using OpenLR.Locations;
+using OpenLR.Model;
 using OpenLR.Referenced;
 using OpenLR.Referenced.Decoding;
+using OsmSharp.Collections.Tags;
+using OsmSharp.Math.Geo;
 using OsmSharp.Routing.Graph;
+using OsmSharp.Units.Distance;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace OpenLR.OsmSharp.Decoding
 {
@@ -13,6 +19,11 @@ namespace OpenLR.OsmSharp.Decoding
         where TReferencedLocation : ReferencedLocation
         where TLocation : ILocation
     {
+        /// <summary>
+        /// Holds the maximum vertex distance.
+        /// </summary>
+        private Meter _maxVertexDistance = 20;
+
         /// <summary>
         /// Holds a dynamic graph.
         /// </summary>
@@ -29,21 +40,336 @@ namespace OpenLR.OsmSharp.Decoding
         }
 
         /// <summary>
-        /// Gets the graph.
-        /// </summary>
-        public DynamicGraphRouterDataSource<TEdge> Graph
-        {
-            get
-            {
-                return _graph;
-            }
-        }
-
-        /// <summary>
         /// Decodes an OpenLR-encoded unreferenced raw OpenLR location into a referenced Location.
         /// </summary>
         /// <param name="location"></param>
         /// <returns></returns>
         public override abstract TReferencedLocation Decode(TLocation location);
+
+        /// <summary>
+        /// Finds all candidate vertex/edge pairs for a given location reference point.
+        /// </summary>
+        /// <param name="lrp"></param>
+        /// <param name="forward"></param>
+        /// <returns></returns>
+        protected virtual SortedSet<CandidateVertexEdge> FindCandidatesFor(LocationReferencePoint lrp, bool forward)
+        {
+            var vertexEdgeCandidates = new SortedSet<CandidateVertexEdge>();
+            var vertexCandidates = this.FindCandidateVerticesFor(lrp);
+            foreach(var vertexCandidate in vertexCandidates)
+            {
+                var edgeCandidates = this.FindCandidateEdgesFor(vertexCandidate.Vertex, forward, lrp.FormOfWay.Value, lrp.FuntionalRoadClass.Value);
+                foreach(var edgeCandidate in edgeCandidates)
+                {
+                    vertexEdgeCandidates.Add(new CandidateVertexEdge()
+                    {
+                        Edge = edgeCandidate.Edge,
+                        Vertex = vertexCandidate.Vertex,
+                        Score = vertexCandidate.Score = edgeCandidate.Score
+                    });
+                }
+            }
+            return vertexEdgeCandidates;
+        }
+
+        /// <summary>
+        /// Finds candidate vertices for a location reference point.
+        /// </summary>
+        /// <param name="lrp"></param>
+        /// <returns></returns>
+        protected virtual IEnumerable<CandidateVertex> FindCandidateVerticesFor(LocationReferencePoint lrp)
+        {
+            // convert to geo coordinate.
+            var geoCoordinate = new GeoCoordinate(lrp.Coordinate.Latitude, lrp.Coordinate.Longitude);
+
+            // build candidates list.
+            var candidates = new HashSet<uint>();
+            var scoredCandidates = new List<CandidateVertex>();
+
+            float latitude, longitude;
+
+            // create a search box.
+            var box = new GeoCoordinateBox(geoCoordinate, geoCoordinate);
+            box = box.Resize(0.1);
+
+            // get arcs.
+            var arcs = _graph.GetArcs(box);
+            foreach (var arc in arcs)
+            {
+                uint vertex = arc.Key;
+                if (!candidates.Contains(vertex))
+                {
+                    _graph.GetVertex(vertex, out latitude, out longitude);
+                    var distance = geoCoordinate.DistanceEstimate(new GeoCoordinate(latitude, longitude));
+                    if (distance.Value < _maxVertexDistance.Value)
+                    {
+                        candidates.Add(vertex);
+                        scoredCandidates.Add(new CandidateVertex()
+                            {
+                                Score = (float)(1.0 - (distance.Value / _maxVertexDistance.Value)),
+                                Vertex = vertex
+                            });
+                    }
+                }
+                vertex = arc.Value.Key;
+                if (!candidates.Contains(vertex))
+                {
+                    _graph.GetVertex(vertex, out latitude, out longitude);
+                    var distance = geoCoordinate.DistanceEstimate(new GeoCoordinate(latitude, longitude));
+                    if (distance.Value < _maxVertexDistance.Value)
+                    {
+                        candidates.Add(vertex);
+                        scoredCandidates.Add(new CandidateVertex()
+                        {
+                            Score = (float)(1.0 - (distance.Value / _maxVertexDistance.Value)),
+                            Vertex = vertex
+                        });
+                    }
+                }
+            }
+            return scoredCandidates;
+        }
+
+        /// <summary>
+        /// Finds candidate edges for a vertex matching a given fow and frc.
+        /// </summary>
+        /// <param name="vertex"></param>
+        /// <param name="forward"></param>
+        /// <param name="fow"></param>
+        /// <param name="frc"></param>
+        /// <returns></returns>
+        protected virtual IEnumerable<CandidateEdge> FindCandidateEdgesFor(uint vertex, bool forward, FormOfWay fow, FunctionalRoadClass frc)
+        {
+            var relevantEdges = new List<CandidateEdge>();
+            foreach (var arc in _graph.GetArcs(vertex))
+            {
+                if (arc.Value.Forward == forward)
+                {
+                    var tags = _graph.TagsIndex.Get(arc.Value.Tags);
+                    var score = this.MatchArc(tags, fow, frc);
+                    if (score > 0)
+                    { // ok, there is a match.
+                        relevantEdges.Add(new CandidateEdge()
+                        {
+                            Score = score,
+                            Edge = arc.Value
+                        });
+                    }
+                }
+            }
+            return relevantEdges;
+        }
+
+        /// <summary>
+        /// Calculates a match between the tags collection and the properties of the OpenLR location reference.
+        /// </summary>
+        /// <param name="tags"></param>
+        /// <param name="fow"></param>
+        /// <param name="frc"></param>
+        /// <returns></returns>
+        protected virtual float MatchArc(TagsCollectionBase tags, FormOfWay fow, FunctionalRoadClass frc)
+        {
+            string highway;
+            if(!tags.TryGetValue("highway", out highway))
+            { // not even a highway tag!
+                return 0;
+            }
+
+            // TODO: take into account form of way? Maybe not for OSM-data?
+
+            switch(frc)
+            { // check there reference values against OSM: http://wiki.openstreetmap.org/wiki/Highway
+                case FunctionalRoadClass.Frc0: // main road.
+                    if(highway == "motorway" || highway == "trunk")
+                    {
+                        return 1;
+                    }
+                    return 0; // no match.
+                case FunctionalRoadClass.Frc1: // first class road.
+                    if (highway == "primary" || highway == "primary_link")
+                    {
+                        return 1;
+                    }
+                    return 0; // no match.
+                case FunctionalRoadClass.Frc2: // second class road.
+                    if (highway == "secondary" || highway == "secondary_link")
+                    {
+                        return 1;
+                    }
+                    return 0; // no match.
+                case FunctionalRoadClass.Frc3: // third class road.
+                    if (highway == "tertiary" || highway == "tertiary_link")
+                    {
+                        return 1;
+                    }
+                    return 0; // no match.
+                case FunctionalRoadClass.Frc4:
+                    if (highway == "road" || highway == "road_link" ||
+                        highway == "unclassified" || highway == "residential")
+                    {
+                        return 1;
+                    }
+                    return 0; // no match.
+                case FunctionalRoadClass.Frc5:
+                    if (highway == "road" || highway == "road_link" ||
+                        highway == "unclassified" || highway == "residential" ||
+                        highway == "living_street")
+                    {
+                        return 1;
+                    }
+                    return 0; // no match.
+                case FunctionalRoadClass.Frc6:
+                    if (highway == "road" || highway == "track" ||
+                        highway == "unclassified" || highway == "residential" ||
+                        highway == "living_street")
+                    {
+                        return 1;
+                    }
+                    return 0; // no match.
+                case FunctionalRoadClass.Frc7: // other class road.
+                    if (highway == "footway" || highway == "bridleway" ||
+                        highway == "steps" || highway == "path" ||
+                        highway == "living_street")
+                    {
+                        return 1;
+                    }
+                    return 0; // no match.
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// Represents a candidate vertex and associated score.
+        /// </summary>
+        public class CandidateVertex
+        {
+            /// <summary>
+            /// Gets or sets the score.
+            /// </summary>
+            public float Score { get; set; }
+
+            /// <summary>
+            /// Gets or sets the vertex.
+            /// </summary>
+            public uint Vertex { get; set; }
+
+            /// <summary>
+            /// Determines whether this object is equal to the given object.
+            /// </summary>
+            /// <param name="obj"></param>
+            /// <returns></returns>
+            public override bool Equals(object obj)
+            {
+                var other = (obj as CandidateVertex);
+                return other != null && other.Vertex == this.Vertex && other.Score == this.Score;
+            }
+
+            /// <summary>
+            /// Serves as a hashfunction.
+            /// </summary>
+            /// <returns></returns>
+            public override int GetHashCode()
+            {
+                return this.Score.GetHashCode() ^
+                    this.Vertex.GetHashCode();
+            }
+        }
+
+        /// <summary>
+        /// Represents a candidate edge and associated score.
+        /// </summary>
+        public class CandidateEdge
+        {
+            /// <summary>
+            /// Gets or sets the score.
+            /// </summary>
+            public float Score { get; set; }
+
+            /// <summary>
+            /// Gets or sets the vertex.
+            /// </summary>
+            public TEdge Edge { get; set; }
+
+            /// <summary>
+            /// Determines whether this object is equal to the given object.
+            /// </summary>
+            /// <param name="obj"></param>
+            /// <returns></returns>
+            public override bool Equals(object obj)
+            {
+                var other = (obj as CandidateEdge);
+                return other != null && other.Edge.Equals(this.Edge) && other.Score == this.Score;
+            }
+
+            /// <summary>
+            /// Serves as a hashfunction.
+            /// </summary>
+            /// <returns></returns>
+            public override int GetHashCode()
+            {
+                return this.Score.GetHashCode() ^
+                    this.Edge.GetHashCode();
+            }
+        }
+
+        /// <summary>
+        /// Represents a candidate vertex/edge pair and associated score.
+        /// </summary>
+        public class CandidateVertexEdge
+        {
+            /// <summary>
+            /// The combined score of vertex and edge.
+            /// </summary>
+            public float Score { get; set; }
+
+            /// <summary>
+            /// Gets or sets the candidate vertex.
+            /// </summary>
+            public uint Vertex { get; set; }
+
+            /// <summary>
+            /// Gets or sets the candidate edge.
+            /// </summary>
+            public TEdge Edge { get; set; }
+
+            /// <summary>
+            /// Determines whether this object is equal to the given object.
+            /// </summary>
+            /// <param name="obj"></param>
+            /// <returns></returns>
+            public override bool Equals(object obj)
+            {
+                var other = (obj as CandidateVertexEdge);
+                return other != null && other.Vertex == this.Vertex && other.Edge.Equals(this.Edge) && other.Score == this.Score;
+            }
+
+            /// <summary>
+            /// Serves as a hashfunction.
+            /// </summary>
+            /// <returns></returns>
+            public override int GetHashCode()
+            {
+                return this.Score.GetHashCode() ^
+                    this.Edge.GetHashCode() ^
+                    this.Vertex.GetHashCode();
+            }
+        }
+        
+        /// <summary>
+        /// A comparer for vertex edge candidates.
+        /// </summary>
+        public class CandidateVertexEdgeComparer : IComparer<CandidateVertexEdge>
+        {
+            /// <summary>
+            /// Compares the two given vertex-edge candidates.
+            /// </summary>
+            /// <param name="x"></param>
+            /// <param name="y"></param>
+            /// <returns></returns>
+            public int Compare(CandidateVertexEdge x, CandidateVertexEdge y)
+            {
+                return x.Score.CompareTo(y.Score);
+            }
+        }
     }
 }
--- OpenLR.OsmSharp/Decoding/LineLocationGraphDecoder.cs
+++ OpenLR.OsmSharp/Decoding/LineLocationGraphDecoder.cs
@@ -1,9 +1,11 @@
 ﻿using OpenLR.Locations;
 using OpenLR.Model;
+using OsmSharp.Collections.Tags;
 using OsmSharp.Math.Geo;
 using OsmSharp.Routing.Graph;
 using OsmSharp.Units.Distance;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace OpenLR.OsmSharp.Decoding
 {
@@ -14,11 +16,6 @@ namespace OpenLR.OsmSharp.Decoding
     public class LineLocationGraphDecoder<TEdge> : GraphDecoder<LineLocationGraph<TEdge>, LineLocation, TEdge>
         where TEdge : IDynamicGraphEdgeData
     {
-        /// <summary>
-        /// Holds the maximum vertex distance.
-        /// </summary>
-        private Meter _maxVertexDistance = 20;
-
         /// <summary>
         /// Creates a line location graph decoder.
         /// </summary>
@@ -36,41 +33,120 @@ namespace OpenLR.OsmSharp.Decoding
         /// <returns></returns>
         public override LineLocationGraph<TEdge> Decode(LineLocation location)
         {
-            // get candidate vertices.
-            var candidateLinks = new List<List<uint>>();
-            candidateLinks.Add(this.FindCandidates(location.First.Coordinate));
-            if(location.Intermediate != null)
+            // get candidate vertices and edges.
+            var candidates = new List<SortedSet<CandidateVertexEdge>>();
+            var lrps = new List<LocationReferencePoint>();
+
+            // loop over all lrps.
+            lrps.Add(location.First);
+            candidates.Add(this.FindCandidatesFor(location.First, true));
+            if (location.Intermediate != null)
             { // there are intermediates.
-                for(int idx = 0; idx < location.Intermediate.Length; idx++)
+                for (int idx = 0; idx < location.Intermediate.Length; idx++)
                 {
-                    candidateLinks.Add(this.FindCandidates(location.Intermediate[idx].Coordinate));
+                    lrps.Add(location.Intermediate[idx]);
+                    candidates.Add(this.FindCandidatesFor(location.Intermediate[idx], true));
                 }
             }
-            candidateLinks.Add(this.FindCandidates(location.Last.Coordinate));
+            lrps.Add(location.Last);
+            candidates.Add(this.FindCandidatesFor(location.Last, true));
 
-            // get candidate edges.
-            var candidateEdges = new List<List<TEdge>>();
+            // find a route between each pair of sequential points.
+            var previous = lrps[0];
+            var previousCandidates = candidates[0];
+            for (int idx = 1; idx < lrps.Count; idx++)
+            {
+                var current = lrps[idx];
+                var currentCandidates = candidates[idx];
+
+                // build a list of combined scores.
+                var combinedScores = new SortedSet<CombinedScore>(new CombinedScoreComparer());
+                foreach (var previousCandidate in previousCandidates)
+                {
+                    foreach (var currentCandidate in currentCandidates)
+                    {
+                        combinedScores.Add(new CombinedScore()
+                            {
+                                Source = previousCandidate,
+                                Target = currentCandidate
+                            });
+                    }
+                }
+
+                object selectedPath = null;
+                while(combinedScores.Count > 0)
+                {
+
+                }
+            }
 
             return null;
         }
 
         /// <summary>
-        /// Returns candidate vertices.
+        /// Represents a combined score.
         /// </summary>
-        /// <param name="coordinate"></param>
-        /// <returns></returns>
-        private List<uint> FindCandidates(Coordinate coordinate)
+        private class CombinedScore
         {
-            // create a search box.
-            var box = new GeoCoordinateBox(
-                new GeoCoordinate(coordinate.Latitude, coordinate.Longitude),
-                new GeoCoordinate(coordinate.Latitude, coordinate.Longitude));
-            box = box.Resize(0.1);
+            /// <summary>
+            /// Gets or sets the source candidate.
+            /// </summary>
+            public CandidateVertexEdge Source { get; set; }
 
-            // get arcs.
-            var arcs = this.Graph.GetArcs(box);
+            /// <summary>
+            /// Gets or sets the target candidate.
+            /// </summary>
+            public CandidateVertexEdge Target { get; set; }
 
-            return null;
+            /// <summary>
+            /// Returns the score.
+            /// </summary>
+            public float Score
+            {
+                get
+                {
+                    return this.Source.Score + this.Target.Score;
+                }
+            }
+
+            /// <summary>
+            /// Determines whether this object is equal to the given object.
+            /// </summary>
+            /// <param name="obj"></param>
+            /// <returns></returns>
+            public override bool Equals(object obj)
+            {
+                var other = (obj as CombinedScore);
+                return other != null && other.Target.Equals(this.Target) && other.Source.Equals(this.Source) && other.Score == this.Score;
+            }
+
+            /// <summary>
+            /// Serves as a hashfunction.
+            /// </summary>
+            /// <returns></returns>
+            public override int GetHashCode()
+            {
+                return this.Score.GetHashCode() ^
+                    this.Target.GetHashCode() ^
+                    this.Source.GetHashCode();
+            }
+        }
+
+        /// <summary>
+        /// A combined score compared.
+        /// </summary>
+        private class CombinedScoreComparer : IComparer<CombinedScore>
+        {
+            /// <summary>
+            /// Compares the two combine scores.
+            /// </summary>
+            /// <param name="x"></param>
+            /// <param name="y"></param>
+            /// <returns></returns>
+            public int Compare(CombinedScore x, CombinedScore y)
+            {
+                return x.Score.CompareTo(y.Score);
+            }
         }
     }
 }
