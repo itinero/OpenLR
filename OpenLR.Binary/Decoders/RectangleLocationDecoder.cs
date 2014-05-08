b5e30f5ae72c06ea0f414d322436988709d60481
xivk <ben@osmsharp.com>
2014-05-08 14:17:35 +0200

    Finished binary decoding of RectangleLocation. Fixes #9.

---
+++ OpenLR.Binary/Decoders/RectangleLocationDecoder.cs
@@ -0,0 +1,24 @@
+﻿using OpenLR.Binary.Data;
+using OpenLR.Locations;
+
+namespace OpenLR.Binary.Decoders
+{
+    /// <summary>
+    /// A decoder that decodes binary data into a rectangle location.
+    /// </summary>
+    public class RectangleLocationDecoder : BinaryDecoder
+    {
+        /// <summary>
+        /// Decodes the given data into a location reference.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        protected override ILocation Decode(byte[] data)
+        {
+            var rectangleLocation = new RectangleLocation();
+            rectangleLocation.LowerLeft = CoordinateConverter.Decode(data, 1);
+            rectangleLocation.UpperRight = CoordinateConverter.DecodeRelative(rectangleLocation.LowerLeft, data, 7);
+            return rectangleLocation;
+        }
+    }
+}
