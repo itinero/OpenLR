5cfd14d2601144b4db238ac85dfc39ab5a288b18
xivk <ben@osmsharp.com>
2014-05-13 12:17:35 +0200

    Finished referenced decoding of PolygonLocation. Closes #17.

---
+++ OpenLR.OsmSharp/Decoding/ReferencedPolygon.cs
@@ -0,0 +1,22 @@
+﻿using OpenLR.Model;
+using OpenLR.Referenced;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenLR.OsmSharp.Decoding
+{
+    /// <summary>
+    /// Represents a referenced polygon location with a graph as a reference.
+    /// </summary>
+    /// <remarks>The reference graph plays no part here, a polygon is just a polygon.</remarks>
+    public class ReferencedPolygon : ReferencedLocation
+    {
+        /// <summary>
+        /// Gets or sets the list of coordinates.
+        /// </summary>
+        public Coordinate[] Coordinates { get; set; }
+    }
+}
---
+++ OpenLR.OsmSharp/Decoding/ReferencedPolygonDecoder.cs
@@ -0,0 +1,44 @@
+﻿using OpenLR.Locations;
+using OpenLR.Model;
+using OsmSharp.Routing.Graph;
+using OsmSharp.Routing.Graph.Router;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenLR.OsmSharp.Decoding
+{
+    /// <summary>
+    /// Represents a referenced polygon location decoder.
+    /// </summary>
+    /// <typeparam name="TEdge"></typeparam>
+    public class ReferencedPolygonDecoder<TEdge> : ReferencedDecoder<ReferencedPolygon, PolygonLocation, TEdge>
+        where TEdge : IDynamicGraphEdgeData
+    {
+        /// <summary>
+        /// Creates a polygon location graph decoder.
+        /// </summary>
+        /// <param name="graph"></param>
+        /// <param name="router"></param>
+        public ReferencedPolygonDecoder(OpenLR.Decoding.Decoder rawDecoder, DynamicGraphRouterDataSource<TEdge> graph, IBasicRouter<TEdge> router)
+            : base(rawDecoder, graph, router)
+        {
+
+        }
+
+        /// <summary>
+        /// Decodes an OpenLR-encoded unreferenced raw OpenLR location into a referenced Location.
+        /// </summary>
+        /// <param name="location"></param>
+        /// <returns></returns>
+        public override ReferencedPolygon Decode(PolygonLocation location)
+        {
+            return new ReferencedPolygon()
+            {
+                Coordinates = location.Coordinates.Clone() as Coordinate[]
+            };
+        }
+    }
+}
