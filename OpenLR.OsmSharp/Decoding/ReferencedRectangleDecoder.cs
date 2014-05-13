43c00f91285477b085b94ce48055da4f1c4a7ed2
xivk <ben@osmsharp.com>
2014-05-13 12:57:22 +0200

    Finished referenced decoding of RectangleLocation. Closes #18.

---
+++ OpenLR.OsmSharp/Decoding/ReferencedRectangleDecoder.cs
@@ -0,0 +1,46 @@
+﻿using OpenLR.Locations;
+using OsmSharp.Routing.Graph;
+using OsmSharp.Routing.Graph.Router;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenLR.OsmSharp.Decoding
+{
+    /// <summary>
+    /// Represents a referenced rectangle location decoder.
+    /// </summary>
+    /// <typeparam name="TEdge"></typeparam>
+    public class ReferencedRectangleDecoder<TEdge> : ReferencedDecoder<ReferencedRectangle, RectangleLocation, TEdge>
+        where TEdge : IDynamicGraphEdgeData
+    {
+        /// <summary>
+        /// Creates a rectangle location graph decoder.
+        /// </summary>
+        /// <param name="graph"></param>
+        /// <param name="router"></param>
+        public ReferencedRectangleDecoder(OpenLR.Decoding.Decoder rawDecoder, DynamicGraphRouterDataSource<TEdge> graph, IBasicRouter<TEdge> router)
+            : base(rawDecoder, graph, router)
+        {
+
+        }
+
+        /// <summary>
+        /// Decodes an OpenLR-encoded unreferenced raw OpenLR location into a referenced Location.
+        /// </summary>
+        /// <param name="location"></param>
+        /// <returns></returns>
+        public override ReferencedRectangle Decode(RectangleLocation location)
+        {
+            return new ReferencedRectangle()
+            {
+                LowerLeftLatitude = location.LowerLeft.Latitude,
+                LowerLeftLongitude = location.LowerLeft.Longitude,
+                UpperRightLatitude = location.UpperRight.Latitude,
+                UpperRightLongitude = location.UpperRight.Longitude
+            };
+        }
+    }
+}
