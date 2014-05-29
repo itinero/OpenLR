66f39d94f9223936d010c888118e75e18770b502
xivk <ben@osmsharp.com>
2014-05-29 14:42:58 +0200

    Added binary decoder base class.

---
+++ OpenLR.Tests/Binary/BinaryDecoderTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenLR.Binary.Decoders;
+using OpenLR.Locations;
+using System;
+
+namespace OpenLR.Tests.Binary
+{
+    /// <summary>
+    /// Contains tests the binary decoder base class.
+    /// </summary>
+    [TestFixture]
+    public class BinaryDecoderTests
+    {
+        /// <summary>
+        /// A simple test decoding from a base64 string.
+        /// </summary>
+        [Test]
+        public void DecodeBase64Test()
+        {
+            double delta = 0.0001;
+
+            var decoder = new CircleLocationDecoder();
+
+            // test null arguments.
+            Assert.Catch<ArgumentNullException>(() => {
+                decoder.Decode(null);
+            });
+
+            // test invalid arguments.
+            Assert.Catch<FormatException>(() =>
+            {
+                decoder.Decode("InvalidCode");
+            });
+        }
+    }
+}
