cb31a3087574753c3e94955b249f2239d3e81f89
xivk <ben@osmsharp.com>
2015-08-26 14:42:02 +0000

    Updated version numbers.

--- SharedAssemblyVersion.cs
+++ SharedAssemblyVersion.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.1.9999")] // semantic versioning Major.Minor.Patch.Build (9999 will be updated by CI server)
+[assembly: AssemblyVersion("0.1.2.9999")] // semantic versioning Major.Minor.Patch.Build (9999 will be updated by CI server)
 [assembly: AssemblyInformationalVersion("Local Build Version")] // do not change this; build server replace this automatically.
