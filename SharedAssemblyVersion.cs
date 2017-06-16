0ab5146d42eaf226273f27dfa354e3f98db50978
Ben Abelshausen <ben@osmsharp.com>
2017-06-16 15:59:11 +0100

    Fixes #79.

--- SharedAssemblyVersion.cs
+++ SharedAssemblyVersion.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.5.1")] // semantic versioning Major.Minor.Patch
-[assembly: AssemblyInformationalVersion("0.5.1-beta")]
+[assembly: AssemblyVersion("0.5.2")] // semantic versioning Major.Minor.Patch
+[assembly: AssemblyInformationalVersion("0.5.2-beta")]
