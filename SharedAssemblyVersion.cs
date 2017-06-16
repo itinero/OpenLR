1fcba142c16c814ec0fc6ac78aeef66023ab5228
Ben Abelshausen <ben@osmsharp.com>
2017-06-16 14:34:46 +0100

    Bumped version #.

--- SharedAssemblyVersion.cs
+++ SharedAssemblyVersion.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.5.0")] // semantic versioning Major.Minor.Patch
-[assembly: AssemblyInformationalVersion("0.5.0-beta")]
+[assembly: AssemblyVersion("0.5.1")] // semantic versioning Major.Minor.Patch
+[assembly: AssemblyInformationalVersion("0.5.1-beta")]
