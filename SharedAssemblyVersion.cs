80e6ad22b00521daffe3b395e7b88ce26f55544c
Ben Abelshausen <ben@osmsharp.com>
2017-12-18 10:23:52 +0000

    Updated everything to .net core and Itinero 1.4.0.

--- SharedAssemblyVersion.cs
+++ SharedAssemblyVersion.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0")] // semantic versioning Major.Minor.Patch
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.1.0")] // semantic versioning Major.Minor.Patch
+[assembly: AssemblyInformationalVersion("1.1.0-pre01")]
