af37ca6c06bf125662119116540bfcb0165ba7c3
xivk <ben@anyways.eu>
2023-01-12 14:29:25 +0100

    Use mutator directly in apply cover.

--- SharedAssemblyVersion.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyVersion("1.1.1")] // semantic versioning Major.Minor.Patch
-[assembly: AssemblyInformationalVersion("1.1.1-pre11")]
--- src/OpenLR/Tools/ApplyCover/RoutingNetworkExtensions.cs
+++ src/OpenLR/Tools/ApplyCover/RoutingNetworkExtensions.cs
@@ -20,70 +20,66 @@ public static class RoutingNetworkExtensions
     /// <summary>
     /// Applies the given edge cover, splits edges accordingly and rewrites tags on covered sections.
     /// </summary>
-    /// <param name="routingNetwork">The routing network.</param>
+    /// <param name="mutator">The routing network mutator.</param>
     /// <param name="edges">The edges.</param>
     /// <param name="rewrite">The callback to rewrite attributes.</param>
-    public static RoutingNetwork ApplyCover(this RoutingNetwork routingNetwork, IEnumerable<(EdgeId edge, bool forward, ushort tailOffset, ushort headOffset)> edges,
+    public static void ApplyCover(this RoutingNetworkMutator mutator,
+        IEnumerable<(EdgeId edge, bool forward, ushort tailOffset, ushort headOffset)> edges,
         RewriteEdgeAttributesFunc rewrite)
     {
-        using (var mutator = routingNetwork.GetAsMutable())
+        // apply per edge.
+        var edgeEnumerator = mutator.GetEdgeEnumerator();
+        foreach (var covered in edges)
         {
-            // apply per edge.
-            var edgeEnumerator = mutator.GetEdgeEnumerator();
-            foreach (var covered in edges)
+            if (!edgeEnumerator.MoveTo(covered.edge, covered.forward)) throw new Exception("Edge not found");
+            if (covered.tailOffset > covered.headOffset)
+                throw new Exception("Tail offset is larger than head offset");
+            if (covered.tailOffset == covered.headOffset) continue;
+
+            var tailShape =
+                ArraySegment<(double longitude, double latitude, float? e)>.Empty as
+                    IEnumerable<(double longitude, double latitude, float? e)>;
+            var tailVertex = edgeEnumerator.Tail;
+            if (covered.tailOffset > 0)
             {
-                if (!edgeEnumerator.MoveTo(covered.edge, covered.forward)) throw new Exception("Edge not found");
-                if (covered.tailOffset > covered.headOffset)
-                    throw new Exception("Tail offset is larger than head offset");
-                if (covered.tailOffset == covered.headOffset) continue;
-
-                var tailShape =
-                    ArraySegment<(double longitude, double latitude, float? e)>.Empty as
-                        IEnumerable<(double longitude, double latitude, float? e)>;
-                var tailVertex = edgeEnumerator.Tail;
-                if (covered.tailOffset > 0)
-                {
-                    tailShape = edgeEnumerator.GetShapeBetween(0, covered.tailOffset).ToList();
-                    var tailLocation = tailShape.Last();
-                    tailVertex = mutator.AddVertex(tailLocation);
-                }
-
-                var headShape =
-                    ArraySegment<(double longitude, double latitude, float? e)>.Empty as
-                        IEnumerable<(double longitude, double latitude, float? e)>;
-                var headVertex = edgeEnumerator.Head;
-                if (covered.headOffset < ushort.MaxValue)
-                {
-                    headShape = edgeEnumerator.GetShapeBetween(covered.headOffset, ushort.MaxValue).ToList();
-                    var headLocation = headShape.First();
-                    headVertex = mutator.AddVertex(headLocation);
-                }
-
-                if (edgeEnumerator.Tail != tailVertex)
-                {
-                    // add tail segment.
-                    mutator.AddEdge(edgeEnumerator.Tail, tailVertex, tailShape, edgeEnumerator.Attributes);
-                }
-
-                if (edgeEnumerator.Tail != tailVertex ||
-                    edgeEnumerator.Head != headVertex)
-                {
-                    // add between segment.
-                    var attributes = rewrite(edgeEnumerator.Attributes, edgeEnumerator.Forward);
-                    var betweenShape = edgeEnumerator.GetShapeBetween(covered.tailOffset, covered.tailOffset);
-                    mutator.AddEdge(tailVertex, headVertex, betweenShape, attributes);
-                }
-
-                if (edgeEnumerator.Head != headVertex)
-                {
-                    // add head segment.
-                    mutator.AddEdge(headVertex, edgeEnumerator.Head, headShape, edgeEnumerator.Attributes);
-                }
-
-                mutator.DeleteEdge(covered.edge);
+                tailShape = edgeEnumerator.GetShapeBetween(0, covered.tailOffset).ToList();
+                var tailLocation = tailShape.Last();
+                tailVertex = mutator.AddVertex(tailLocation);
             }
-        }
 
-        return routingNetwork.RouterDb.Latest;
+            var headShape =
+                ArraySegment<(double longitude, double latitude, float? e)>.Empty as
+                    IEnumerable<(double longitude, double latitude, float? e)>;
+            var headVertex = edgeEnumerator.Head;
+            if (covered.headOffset < ushort.MaxValue)
+            {
+                headShape = edgeEnumerator.GetShapeBetween(covered.headOffset, ushort.MaxValue).ToList();
+                var headLocation = headShape.First();
+                headVertex = mutator.AddVertex(headLocation);
+            }
+
+            if (edgeEnumerator.Tail != tailVertex)
+            {
+                // add tail segment.
+                mutator.AddEdge(edgeEnumerator.Tail, tailVertex, tailShape, edgeEnumerator.Attributes);
+            }
+
+            if (edgeEnumerator.Tail != tailVertex ||
+                edgeEnumerator.Head != headVertex)
+            {
+                // add between segment.
+                var attributes = rewrite(edgeEnumerator.Attributes, edgeEnumerator.Forward).ToList();
+                var betweenShape = edgeEnumerator.GetShapeBetween(covered.tailOffset, covered.tailOffset);
+                mutator.AddEdge(tailVertex, headVertex, betweenShape, attributes);
+            }
+
+            if (edgeEnumerator.Head != headVertex)
+            {
+                // add head segment.
+                mutator.AddEdge(headVertex, edgeEnumerator.Head, headShape, edgeEnumerator.Attributes);
+            }
+
+            mutator.DeleteEdge(covered.edge);
+        }
     }
 }
